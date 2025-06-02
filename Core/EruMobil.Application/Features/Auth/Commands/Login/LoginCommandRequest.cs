8786c2cb1b71ed52d37e6768f0715d26d60c1d17
OkanT6 <okan.tpdmr.dev@outlook.com>
2025-06-02 14:53:26 +0300

    2Mayıs_Login&DeviceFeaturesAreUpdatedAndFixed

--- Core/EruMobil.Application/Features/Auth/Commands/Login/LoginCommandRequest.cs
+++ Core/EruMobil.Application/Features/Auth/Commands/Login/LoginCommandRequest.cs
@@ -15,5 +15,21 @@ namespace EruMobil.Application.Features.Auth.Commands.Login
         [DefaultValue("Okan1234")]
 
         public string Password { get; set; }
+
+
+        [DefaultValue("your_DeviceName_value")]
+
+        public string DeviceName { get; set; }
+        [DefaultValue("your_UniqueDeviceIdentifier_value")]
+        public string UniqueDeviceIdentifier { get; set; }
+        [DefaultValue("your_Platform_value")]
+
+        public string Platform { get; set; } // "android" veya "ios"
+        [DefaultValue("your_FcmTokene_value")]
+
+        public string FcmToken { get; set; }
+        [DefaultValue("your_AppVersion_value")]
+
+        public string AppVersion { get; set; }
     }
 }
--- Core/EruMobil.Application/Features/Auth/Rules/AuthRules.cs
+++ Core/EruMobil.Application/Features/Auth/Rules/AuthRules.cs
@@ -1,6 +1,10 @@
 ﻿using EruMobil.Application.Bases;
+using EruMobil.Application.Features.Auth.Commands.Login;
 using EruMobil.Application.Features.Auth.Exceptions;
+using EruMobil.Application.Features.Devices.Commands.RegisterDevice;
+using EruMobil.Application.Interfaces.UnitOfWorks;
 using EruMobil.Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +15,14 @@ namespace EruMobil.Application.Features.Auth.Rules
 {
     public class AuthRules : BaseRules
     {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMediator mediator;
+
+        public AuthRules(IUnitOfWork unitOfWork,IMediator mediator)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mediator = mediator;
+        }
         public Task UserShouldNotBeExisted(User user, string registerTypeTarget)
         {
             if (user is not null && registerTypeTarget == user.UserType) throw new UserAlreadyExistsException();
@@ -35,6 +47,39 @@ namespace EruMobil.Application.Features.Auth.Rules
             if (user is null) throw new UserNotFoundException();
             return Task.CompletedTask;
         }
+
+        public async Task LoginedDeviceMustBeMatchedOrNotRegistered(User user, LoginCommandRequest loginCommandRequest)
+        {
+
+            
+            Device device = await unitOfWork.GetReadRepository<Device>().GetAsync(Device => Device.UniqueDeviceIdentifier == loginCommandRequest.UniqueDeviceIdentifier);
+
+            if (device is null)
+            {
+                // Device bulunamadı, yeni device kaydet
+                var registerDeviceCommandRequest = new RegisterDeviceCommandRequest
+                {
+                    UserId = user.Id,
+                    UniqueDeviceIdentifier = loginCommandRequest.UniqueDeviceIdentifier,
+                    DeviceName = loginCommandRequest.DeviceName, // veya parametre olarak alabilirsiniz
+                    Platform = loginCommandRequest.Platform, // veya User-Agent'tan çıkarabilirsiniz
+                    FcmToken = loginCommandRequest.FcmToken, // İlk kayıtta boş, sonra update edilebilir
+                    AppVersion = loginCommandRequest.AppVersion // veya parametre olarak alabilirsiniz
+                };
+
+                await mediator.Send(registerDeviceCommandRequest);
+
+
+            }
+            else if (device.UserId==user.Id)
+            {
+                return;            }
+
+            else         
+                throw new LoginedDeviceMustBeMatchedOrNotRegisteredException();   
+
+        }
+
 
     }
 }
